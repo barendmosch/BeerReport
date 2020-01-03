fb8c23631d0fdb7e40d7d52f6b282a285eeab5b3
barendmosch <moschbarend@gmail.com>
2020-01-03 15:40:43 +0100

    second commit

--- src/cs/SomeFunction.cs
+++ src/cs/SomeFunction.cs
@@ -12,7 +12,7 @@ namespace Company.Function {
     public static class SomeFunction {
         [FunctionName("SomeFunction")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req, ILogger log) {
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req, ILogger log) {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
             string name = req.Query["name"];
