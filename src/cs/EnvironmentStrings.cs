c16102f26ef0d17ba38f3f894f4964f1278ad980
barendmosch <moschbarend@gmail.com>
2020-01-15 23:36:43 +0100

    added storage connection string

--- src/cs/EnvironmentStrings.cs
+++ src/cs/EnvironmentStrings.cs
@@ -2,6 +2,6 @@
 namespace BeerAppServerSide {
     public class EnvironmentStrings{
         public readonly static string BlobStorage = "bier-container";
-        public readonly static string StorageAccount = "";
+        public readonly static string StorageAccount = "DefaultEndpointsProtocol=https;AccountName=storageaccountserve9860;AccountKey=VhTGhtO4vd2seOlbB3RvpRi9eCc/vTshPz1ewFI7CBTC8I9YzRcBzsp9Au0+DJzhDPpaUCDH7uY/4rUVKUzQXw==;BlobEndpoint=https://storageaccountserve9860.blob.core.windows.net/;QueueEndpoint=https://storageaccountserve9860.queue.core.windows.net/;TableEndpoint=https://storageaccountserve9860.table.core.windows.net/;FileEndpoint=https://storageaccountserve9860.file.core.windows.net/;";
     }
 }
